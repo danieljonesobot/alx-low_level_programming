dbb7b9ab9f4cbbdcbbbc5ff39a0a1419aa96825a
danieljonesobot <daniel.jonesobot@gmail.com>
2023-03-17 04:17:01 -0700

    fix to task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - entry point prints Programming is like building a multilingual puzzle
  * Return: always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle.\n");
+puts("Programming is like building a multilingual puzzle.\n");
 return (0);
 }
