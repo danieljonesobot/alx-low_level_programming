9a0333f14d0265e82a520f72d9f2e5d895ca23dc
danieljonesobot <daniel.jonesobot@gmail.com>
2023-03-17 08:59:02 -0700

    first commit for task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+  char c;
+  int i;
+  long int l;
+  long long int h;
+  float f;
+  
+  printf("The size of char is:%lu\n", (unsigned long) sizeof(c));
+  printf("The size of int is:%lu\n", (unsigned long) sizeof(i));
+  printf("The size of long int is:%lu\n", (unsigned long) sizeof(l));
+  printf("The size of long long int is:%lu\n", (unsigned long) sizeof(h));
+  printf("The size of float is:%lu\n", (unsigned long) sizeof(f));
+return (0);
+}
