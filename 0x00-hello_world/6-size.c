83fa7fba08bfe2617165fec02785729f98689c1f
danieljonesobot <daniel.jonesobot@gmail.com>
2023-03-20 03:55:46 -0700

    fix for task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,11 @@ char c;
 int i;
 long int l;
 long long int h;
-float f;  
-printf("The size of char is:%lu\n", (unsigned long) sizeof(c));
-printf("The size of int is:%lu\n", (unsigned long) sizeof(i));
-printf("The size of long int is:%lu\n", (unsigned long) sizeof(l));
-printf("The size of long long int is:%lu\n", (unsigned long) sizeof(h));
-printf("The size of float is:%lu\n", (unsigned long) sizeof(f));
+float f;
+printf("The size of char is:%lu byte(s)\n", (unsigned long) sizeof(c));
+printf("The size of int is:%lu byte(s)\n", (unsigned long) sizeof(i));
+printf("The size of long int is:%lu byte(s)\n", (unsigned long) sizeof(l));
+printf("The size of long long int is:%lu byte(s)\n", (unsigned long) sizeof(h));
+printf("The size of float is:%lu byte(s)\n", (unsigned long) sizeof(f));
 return (0);
 }
