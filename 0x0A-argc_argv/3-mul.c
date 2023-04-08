8ba2442b496464e1290c2c22e7e1c1e2458f79d9
danieljonesobot <daniel.jonesobot@gmail.com>
2023-04-08 11:52:19 -0700

    first commit task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program that takes first two integer arguments and prints the product
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if not enough arguments passed in, 0 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, product = 1;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		
+		for (i = 1; i < argc; i++)
+		product *= atoi(argv[i]);
+
+		printf("%d\n", product);
+	}
+
+	return (0);
+}
