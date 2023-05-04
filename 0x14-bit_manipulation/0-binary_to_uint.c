8193aee32f7c4aff7de97dd6f95bf57f5e6ed433
danieljonesobot <daniel.jonesobot@gmail.com>
2023-05-04 11:55:21 -0700

    fix for task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,12 +5,14 @@
  *
  * @b: pointer to a string.
  *
- * Return: converted number (int) OR 0 if there is one or more chars in the string b that is not 0 or 1
+ * Return: converted number (int)
+ * OR 0 if there is one or more chars in the string b that is not 0 or 1
+ * b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int decimal = 0;
-	unsigned int multiplayer = 1;
+	unsigned int multiplier = 1;
 	int i, j;
 
 	if (b == NULL)
@@ -26,10 +28,10 @@ unsigned int binary_to_uint(const char *b)
 
 		if (b[j] == '1')
 		{
-			decimal += multiplayer;
+			decimal += multiplier;
 		}
 
-		multiplayer *= 2;
+		multiplier *= 2;
 	}
 
 	return (decimal);
