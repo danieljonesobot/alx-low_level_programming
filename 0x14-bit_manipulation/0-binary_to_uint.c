266518b16a6c9e2ead0e13d584b4bc801f05a573
danieljonesobot <daniel.jonesobot@gmail.com>
2023-05-04 11:33:24 -0700

    created main.h file and task 0 files

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - a program that converts a binary to unsigned int.
+ *
+ * @b: pointer to a string.
+ *
+ * Return: converted number (int) OR 0 if there is one or more chars in the string b that is not 0 or 1
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0;
+	unsigned int multiplayer = 1;
+	int i, j;
+
+	if (b == NULL)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		if (b[j] != '1' && b[j] != '0')
+			return (0);
+
+		if (b[j] == '1')
+		{
+			decimal += multiplayer;
+		}
+
+		multiplayer *= 2;
+	}
+
+	return (decimal);
+}
